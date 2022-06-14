24f9e706edf3e2a9c8ade473fcef3437ea64c5a2
Mario Munoz <pythonbynight@gmail.com>
2022-06-14 16:19:53 -0700

    fix app router pointer

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 from app.mylib.util import incl_static
-from app.routes import router
+from app.routes import web_router
 
 
 def get_app() -> FastAPI:
@@ -9,7 +9,7 @@ def get_app() -> FastAPI:
     app = FastAPI(title="Rock Paper Scissors", version="0.0.1")
 
     incl_static(app)  # Mount static files in /static directory
-    app.include_router(router)  # Register routes in `routes` module
+    app.include_router(web_router)  # Register routes in `routes` module
     return app
 
 
